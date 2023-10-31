2c9ce9e0a9cb0688bd9d49c5ce1acd52ccb61c67
Vladyslavua <vladgershman@gmail.com>
2023-10-31 18:16:00 +0100

    Init job tracker project

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import './globals.css'
+import React from "react";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import {ClerkProvider} from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+    title: 'Admin Dashboard',
+    description: 'Admin Dashboard',
+}
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+
+    return (
+        <ClerkProvider>
+            <html lang="en">
+            <body className={inter.className}>
+            {children}
+            </body>
+            </html>
+        </ClerkProvider>
+    )
+}
